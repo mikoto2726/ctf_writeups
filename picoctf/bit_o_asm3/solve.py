eb8b98fae1f9ef245f0a93388007ec38b124bbdd
s1f10220137 <s1f102201379@iniad.org>
2024-09-05 03:01:44 -0400

    assembly

---
+++ picoctf/bit_o_asm3/solve.py
@@ -0,0 +1,5 @@
+print(int(0x9fe1a))
+print(int(0x4))
+print(int(0x1f5))
+
+print(int(0x9fe1a) * int(0x4) + int(0x1f5))
---
+++ picoctf/bit_o_asm4/solve.py
@@ -0,0 +1,12 @@
+a = int(0x9fe1a)
+b = int(0x2710)
+
+print(a)
+print(b)
+
+if a <= b:
+    c = a + int(0x65)
+else:
+    c = a - int(0x65)
+
+print(c)
