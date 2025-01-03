f780b36a249db615f8057c86936d9cc39d830cef
mikoto2726 <mikoto27260@gmail.com>
2025-01-03 21:28:33 +0900

    fix

---
+++ picoctf/1_wanna_b3_a_r0ck5tar/code.py
@@ -0,0 +1,24 @@
+
+a_guitar = 10
+Music = 26
+the_music = int(input("Enter the music: "))
+
+if the_music == a_guitar:
+    print("Keep on rocking!")
+    the_rhythm = int(input("Enter the rhythm: "))
+    
+    if the_rhythm - Music == 0:
+        Tommy = 66
+        print(chr(Tommy))  # ASCII変換
+        Music = 79
+        Jamming = 78
+        print(chr(Music))  # ASCII変換
+        print(chr(Jamming))  # ASCII変換
+        Tommy = 74
+        print(chr(Tommy))  # ASCII変換
+        Rock = 86
+        print(chr(Rock))  # ASCII変換
+        Tommy = 73
+        print(chr(Tommy))  # ASCII変換
+    else:
+        print("That ain't it, Chief")
